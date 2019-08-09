44268de1288128eb25e8f627e514730e22cd8d82
kosoviankrossover <meritonibrahimi@Meritons-MBP-52.lan1>
2019-08-09 16:45:12 -0700

    user defiend functions, for loops, and pointers

--- c/simple1.c
+++ c/simple1.c
@@ -1,12 +1,62 @@
-// incl
+// this is a one line comment
+
+// include a header file
 #include <stdio.h>
 
+// define an alias
 #define SUCCESS 0
 
+/* This is a multi-line comment.
+We are declaring a helper function that will provide some function for our
+main program. The int before the function  declaration indicates that we expect
+to return an integer. */
+int helper_function(int a, int b);
+
+/* This is the main function of the program. */
 int main(){
-	int i = 8;
-	for (int j=0; j<3; j++){
-		printf("%d + %d = %d\n",i,j,i+j);
-	}
+
+	// // simple for loop addition
+	// int i = 8;
+	// for (int j=0; j<3; j++){
+	// 	printf("%d + %d = %d\n",i,j,i+j);
+	// }
+
+	// declare and define some integers
+	int i = 10;
+	int j = 20;
+
+	// use our helper function to add the two variables
+	int sum = helper_function(i,j);
+
+	// print the result
+	printf("i+j = %d \n", helper_function(i,j));
+
+	// a pointer
+	int* ptr;
+
+	// let ptr point towards the location holding i
+	ptr = &i;
+	// print the dereferenced ptr
+	printf("The value at the dereferenced ptr: %d \n", *ptr);
+
+	// let ptr point towards the location holding j
+	ptr = &j;
+	// print the dereferenced ptr
+	printf("The value at the dereferenced ptr: %d \n", *ptr);
+
+	// lets store something else to where ptr points
+	*ptr = 30;
+	printf("The value at the dereferenced ptr: %d \n", *ptr);
+
+	// whats store in j now tho?
+	printf("j = %d \n", j);
+
+
 	return(SUCCESS);
+}
+
+
+// the helper function
+int helper_function(int a, int b){
+	return(a+b);
 }
