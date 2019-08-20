a35801bcd5ff8de911c311a4f239724c5bc94e02
kosoviankrossover <meritonibrahimi@Meritons-MBP-52.lan1>
2019-08-20 15:12:32 -0700

    combined files for simplicity

--- c/simple1.c
+++ c/simple1.c
@@ -4,34 +4,95 @@
 // this header in particular includes the printf function that we often use
 #include <stdio.h>
 
+// header file containing the bool type
+#include <stdbool.h>
+
 // define an alias
 #define SUCCESS 0
 
 /* This is a multi-line comment.
 We are declaring a helper function that will provide some function for our
-main program. The int before the function  declaration indicates that we expect
+main program. The int before the function declaration indicates that we expect
 to return an integer. */
 int helper_function(int a, int b);
 
 /* This is the main function of the program. */
 int main(){
 
-	// // simple for loop addition
-	// int i = 8;
-	// for (int j=0; j<3; j++){
-	// 	printf("%d + %d = %d\n",i,j,i+j);
-	// }
-
-	// declare and define some integers
-	int i = 10;
-	int j = 20;
+     // declare an integer
+     int i = 8;
+    
+    // simple for loop addition
+    printf("using a loop....\n");
+    printf("i + j -- looping over j\n");
+     for (int j=0; j<3; j++){
+         printf("%d + %d = %d\n",i,j,i+j);
+     }
+    
+    printf("\n");
 
 	// use our helper function to add the two variables
-	int sum = helper_function(i,j);
-
+    int j = 4;
+    int sum;
+    sum = helper_function(i,j);
 	// print the result
-	printf("i+j = %d \n", helper_function(i,j));
+    printf("using a helper function to add....\n");
+	printf("%d + %d = %d \n", i, j, sum);
 
+    printf("\n");
+    
+    // octal notaion - base 8 numbers
+    // in base 8, the number 40 is expressed as 50
+    // to declare a variable in base 8, use a preceeding 0
+    int octal = 050;
+    printf("The number \'%o\' in base 8 is equal to %d in base 10\n", octal, octal);
+    
+    printf("\n");
+    
+    // floating point values
+    float flt_ex = 3.87;
+    printf("This is me printing a float: \'%f\'\n", flt_ex);
+    printf("Printing only the first two decimal values: %.2f\n", flt_ex);
+    
+    printf("\n");
+    
+    // boolean and some if statements
+    printf("boolean condtionals...\n");
+    bool boolvar1 = false;
+    bool boolvar2 = true;
+    if (boolvar1) {
+        printf("boolvar1 is true\n");
+    }
+    else if (boolvar2) {
+        printf("Boolvar2 is true\n");
+    }
+    else{
+        printf("Neither bool is true\n");
+    }
+    
+    printf("\n");
+    
+    // integer and float math
+    printf("integer and float math....\n");
+    float f1, f2;
+    int i1, i2;
+    
+    i1 = 156;
+    f1 = 10.367;
+    
+    i2 = f1;
+    printf("float into int %d\n", i2);
+    f2 = i1;
+    printf("int into float %f\n", f2);
+    
+    f2 = i1/100;
+    printf("%d / 100 = %f\n", i1, f2);
+    
+    f2 = i1/100.0;
+    printf("%d / 100.0 = %f\n", i1, f2);
+    
+    printf("\n");
+    
 	// a pointer
 	int* ptr;
 
@@ -51,7 +112,6 @@ int main(){
 
 	// whats store in j now tho?
 	printf("j = %d \n", j);
-
 
 	return(SUCCESS);
 }
