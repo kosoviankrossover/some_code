509391f286741ad9066473d2f875f39d112f8377
kosoviankrossover <meritonibrahimi@Meritons-MBP-52.lan1>
2019-09-06 02:54:08 -0700

    clarified const pointers; added dealing with pointers to array; added dealing with matrices

--- c/pointer.c
+++ c/pointer.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 
+void pointers_to_array (void);
+
+void matrix (void);
+
 int main(void){
     // a pointer
     printf("pointers...\n");
@@ -36,9 +40,6 @@ int main(void){
     printf("whats stored in j now tho?\n");
     printf("j = %d \n", j);
     
-    
-    
-    
     /*
      We can change the pointer to point to any other integer variable,
      but cannot change the value of the object (entity) pointed using
@@ -48,10 +49,73 @@ int main(void){
     printf("constant pointer...\n");
     
     // constant pointer - this is fine
+    /*
+     const int* ptr;
+     Declares ptr a pointer to const int type.
+     You can modify ptr itself but the object pointed to by ptr shall not be modified.
+     */
     const int *ptr_ = &i;
     printf("ptr: %d\n", *ptr_);
     // but this produces an error
     // *ptr = 100;
     
+    printf("\n\n");
+    
+    printf("pointers to array \n");
+    pointers_to_array();
+    
+    printf("\n\n");
+    
+    printf("matrix \n");
+    matrix();
+    
     return(0);
+}
+
+void pointers_to_array (void) {
+    int arr[] = { 3, 5, 6, 7, 9 };
+    int* p = arr;
+    int (*ptr)[5] = &arr;
+    
+    printf("p = %p, ptr = %p\n", p, ptr);
+    printf("*p = %d, *ptr = %p\n", *p, *ptr);
+    
+    printf("sizeof(p) = %lu, sizeof(*p) = %lu\n",
+           sizeof(p), sizeof(*p));
+    printf("sizeof(ptr) = %lu, sizeof(*ptr) = %lu\n",
+           sizeof(ptr), sizeof(*ptr));
+    printf("second element of array: arr[1] or *(p+1) or *((*ptr)+1) = %d,%d,%d \n", arr[1], *(p+1), *((*ptr)+1));
+    
+    return;
+}
+
+void matrix (void) {
+    // vars
+    int i;
+    int j;
+    
+    // matrix
+    int arr[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+    for (i=0; i<3; i++) {
+        for (j=0; j<4; j++) {
+            printf("%d      ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("arr : points to first row - %p \n", arr);
+    printf("(arr+1) : points to second row - %p \n", (arr+1));
+    printf("*(arr+1)+2 : points third element of the second row - %p \n", *(arr+1)+2);
+    
+    printf("\n");
+    
+    printf("**arr : represents the first element of the first row - %d \n", **arr);
+    printf("**(arr+1) : represents the first element of the second row - %d \n", **(arr+1));
+    printf("*(*(arr+1)+2) : represents the third element of the second row - %d \n", *(*(arr+1)+2));
+    
+    return;
 }
